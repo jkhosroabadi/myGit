902db57c3f655732e631eb7d275de94912f4d312
jkhosroabadi <78465668+jkhosroabadi@users.noreply.github.com>
2022-08-13 09:31:08 +0430

    Update Test.java

--- src/nocr/Test.java
+++ src/nocr/Test.java
@@ -4,5 +4,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("salam123456 789 111 999999   10000 1 22");
+        System.out.println("bye");
     }
 }
