522a4e7617a2f3ea42beab21f6e2bcff9937597e
java khosroabadi <j_khosroabadi@yahoo.com>
2022-08-13 09:26:14 +0430

    change 12

--- src/nocr/Test.java
+++ src/nocr/Test.java
@@ -3,6 +3,6 @@ package nocr;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("salam123456 789 111 999999   10000 1");
+        System.out.println("salam123456 789 111 999999   10000 1 22");
     }
 }
